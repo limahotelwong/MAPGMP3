6dec1c70a263ee0a78dde30593e7510222b91a47
Wong Liheng <s10146112@connect.np.edu.sg>
2015-02-03 13:43:21 +0800

    New

---
+++ app/src/main/java/edu/np/ece/mapg/mp3/Facebook.java
@@ -0,0 +1,21 @@
+package edu.np.ece.mapg.mp3;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Liheng on 3/2/2015.
+ */
+public class Facebook extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        View view = inflater.inflate(R.layout.facebook, container, false);
+        return view;
+    }
+
+}
